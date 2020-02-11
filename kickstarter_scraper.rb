89740a6ad59a94337f275ca86e41dc1a1c9a4214
Micah Banschick <micha.ban@gmail.com>
2020-02-11 14:24:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,12 @@
 require "nokogiri"
 require "pry"
 
+# projects: kickstarter.css("li.project.grid_4")
+# title: projects.css("h2.bbcard_name strong a").text
+# image_link: projects.css("div.project-thumbnail a img").attribute("src").value
+# description: projects.css("p.bbcard_blurb").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text
+
 def create_project_hash
 =begin   
   :projects => {
@@ -21,10 +27,6 @@ def create_project_hash
 html = File.read('fixtures/kickstarter.html')
  
 kickstarter = Nokogiri::HTML(html)
-# projects: kickstarter.css("li.project.grid_4")
-# title: projects.css("h2.bbcard_name strong a").text
-# image_link: projects.css("div.project-thumbnail a img").attribute("src").value
-# description: projects.css("p.bbcard_blurb").text
 
 binding.pry
 end
