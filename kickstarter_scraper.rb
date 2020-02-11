05d2d59ef008585a17b26ae05095dc5c2818971c
Micah Banschick <micha.ban@gmail.com>
2020-02-11 13:46:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,20 @@
 # require libraries/modules here
 
 def create_project_hash
-  # write your code here
+  
+  :projects => {
+  "My Great Project"  => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  },
+  "Another Great Project" => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  }
+}
+
 end
