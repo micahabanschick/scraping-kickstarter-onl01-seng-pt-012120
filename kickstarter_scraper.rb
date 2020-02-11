c05807b119ba03d36dd529f46088f242a5decc15
Micah Banschick <micha.ban@gmail.com>
2020-02-11 14:20:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -22,9 +22,9 @@ html = File.read('fixtures/kickstarter.html')
  
 kickstarter = Nokogiri::HTML(html)
 # projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image_link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
+# title: projects.css("h2.bbcard_name strong a").text
+# image_link: projects.css("div.project-thumbnail a img").attribute("src").value
+# description: projects.css("p.bbcard_blurb").text
 
 binding.pry
 end
